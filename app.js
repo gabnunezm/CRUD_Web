ef977750ed909b3e42c32d81c926d65b6e4c6ceb
Gabriel Nuñez <20231871@itla.edu.do>
2025-04-03 12:04:57 -0400

    feat: implement vehicle rendering functionality in app.js

--- app.js
+++ app.js
@@ -25,5 +25,27 @@ form.addEventListener('submit', (e) => {
 
     localStorage.setItem('vehicles', JSON.stringify(vehicles));
     form.reset();
-    
+    renderVehicles();
 });
+
+// Mostrar vehículos
+function renderVehicles() {
+    vehicleList.innerHTML = vehicles.map(vehicle => `
+        <tr class="border text-center">
+            <td class="p-2 border">${vehicle.brand}</td>
+            <td class="p-2 border">${vehicle.model}</td>
+            <td class="p-2 border">${vehicle.year}</td>
+            <td class="p-2 border">$${vehicle.price}</td>
+            <td class="p-2 border">${vehicle.color}</td>
+            <td class="p-2 border">
+                <button onclick="editVehicle('${vehicle.id}')" class="bg-yellow-500 text-white px-2 py-1 rounded mr-2">
+                    Editar
+                </button>
+                <button onclick="deleteVehicle('${vehicle.id}')" class="bg-red-500 text-white px-2 py-1 rounded">
+                    Eliminar
+                </button>
+            </td>
+        </tr>
+    `).join('');
+}
+renderVehicles();
